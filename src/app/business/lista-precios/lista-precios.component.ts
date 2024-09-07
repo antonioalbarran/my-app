ed936bf1368175976bd73e33773bf0441129941b
antonio@albarran.com.mx <antonio@albarran.com.mx>
2024-09-07 15:03:07 -0600

    Se modifica el acomodo de la lista de precios

--- src/app/business/lista-precios/lista-precios.component.ts
+++ src/app/business/lista-precios/lista-precios.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
@@ -13,17 +13,94 @@ import { AuthService } from '../../core/services/auth.service';
   templateUrl: './lista-precios.component.html',
   styleUrl: './lista-precios.component.css'
 })
-export default class ListaPreciosComponent {
-  userName  = 'Antonio Reynoso';
-  title = 'Lista de Precios';
-  private token = '';
-  productos = [];
-  constructor(private service: AuthService,private listaPreciosService: ListaPreciosService) {
-     
+export default class ListaPreciosComponent implements OnInit {
+  //userName  = 'Antonio Reynoso';
+  //title = 'Lista de Precios';
+  //private token = '';
+  productos: any[] = [];
+
+  conIVA = true;
+  bntIVAText="Con IVA";
+  conServicios = true;
+  bntServiciosText="Con Servicios";
+  conExistencia = false;
+  bntExistenciaText="Con Existencia";
+  unaLlanta = true;
+  dosLlantas = false;
+  tresLlantas = false;
+  cuatroLlantas = false;
+
+  constructor(private service: AuthService,private listaPreciosService: ListaPreciosService) {}
+
+  //productos$ = this.listaPreciosService.getAllProducts(this.service.getToken()??'',44);
+
+  ngOnInit(): void {
+    this.listaPreciosService.getProduct().subscribe(productos => {
+      this.productos = productos||[];
+    })
   }
-  
-  productos$ = this.listaPreciosService.getAllProducts(this.service.getToken()??'',44);
-  
+  bnt_iva(){
+    this.conIVA = !this.conIVA
+    console.log(this.conIVA?"bnt_iva activado":"bnt_iva desactivado");
+    this.bntIVAText = this.conIVA?"Con IVA":"Sin IVA";
+    this.productos.map(producto => {
+      if(this.conIVA){
+        producto.precio_max = producto.precio_base * 1.16;
+        producto.precio = producto.precio_25 * 1.16;
+        producto.precio_intel = producto.precio_inter * 1.16;
+        producto.precio_3 = producto.precio_3_meses * 1.16;
+      }else{
+        producto.precio_max = producto.precio_base;
+        producto.precio = producto.precio_25;
+        producto.precio_intel = producto.precio_inter;
+        producto.precio_3 = producto.precio_3_meses;
+      }
+    })
+  }
+  bnt_servicios(){
+    this.conServicios = !this.conServicios
+    console.log(this.conServicios?"bnt_servicios activado":"bnt_servicios desactivado");
+    this.bntServiciosText = this.conServicios?"Con Servicios":"Sin Servicios";
+  }
+  bnt_existencia(){
+    this.conExistencia = !this.conExistencia
+    console.log(this.conExistencia?"bnt_existencia activado":"bnt_existencia desactivado");
+    this.bntExistenciaText = this.conExistencia?"Con Existencia":"Sin Existencia";
+  }
+
+  bnt_llantas(cantdadeLlantas: number){
+    console.log(cantdadeLlantas);
+    switch(cantdadeLlantas){
+      case 1:
+        this.unaLlanta = true;
+        this.dosLlantas = false;
+        this.tresLlantas = false;
+        this.cuatroLlantas = false;
+        break;
+      case 2:
+        this.unaLlanta = false;
+        this.dosLlantas = true;
+        this.tresLlantas = false;
+        this.cuatroLlantas = false;
+        break;
+      case 3:
+        this.unaLlanta = false;
+        this.dosLlantas = false;
+        this.tresLlantas = true;
+        this.cuatroLlantas = false;
+        break;
+      case 4:
+        this.unaLlanta = false;
+        this.dosLlantas = false;
+        this.tresLlantas = false;
+        this.cuatroLlantas = true;
+        break;
+    }
+  }
+
+  calculaPrecio(producto: any){
+    return producto;
+  }
 
   getClassCSS(id_clasificacion: number) {
     switch (id_clasificacion) {
--- src/app/business/lista-precios/lista-precios.service.ts
+++ src/app/business/lista-precios/lista-precios.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,33 @@ export class ListaPreciosService {
       'Authorization': `Bearer ${token}`
     }
     this.urlListaDePrecios = this.urlListaDePrecios + id_sucursal;
-    console.log(this.urlListaDePrecios);
-    return this.http.get<any>(this.urlListaDePrecios,{'headers':headers})
+    //console.log(this.urlListaDePrecios);
+    //return this.http.get<any>(this.urlListaDePrecios,{'headers':headers})
+    return new Observable();
+  }
+
+  getProduct(): Observable<any[]> {
+    const producto=[
+        {nombre:"Llanta 205 70 R14 BRAVURIS 5HM BARUM",
+          codigo:"05E0916O",
+          precio_max:1845,
+          precio:1384,
+          precio_intel:1273,
+          precio_3:1384,
+
+          precio_base:1845,
+          precio_25:1384,
+          precio_iter:1273,
+          precio_3_meses:1384,
+
+          existencia:0,
+          red:4,
+          promocion:"",
+          codigo_albarran:"970 414 30",
+          id_line:5,
+          id_clasificacion:2
+        }
+      ];
+      return of(producto);
   }
 }
